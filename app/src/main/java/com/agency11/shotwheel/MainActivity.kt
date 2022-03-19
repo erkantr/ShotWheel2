05508580a808e2fad8e408caf0cbc3266cb687e9
Erkan <erkanm11t@gmail.com>
2022-03-19 22:13:23 +0300

    Initial commit

---
+++ app/src/main/java/com/agency11/shotwheel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.agency11.shotwheel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
