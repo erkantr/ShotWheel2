4604bc7c661c9b2f85b4a60459b3640beb1d2173
Erkan <erkanm11t@gmail.com>
2022-03-22 23:33:44 +0300

    commit 2

--- app/src/main/java/com/agency11/shotwheel/MainActivity.kt
+++ app/src/main/java/com/agency11/shotwheel/MainActivity.kt
@@ -2,12 +2,14 @@ package com.agency11.shotwheel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)  
 
         println("inş")
+        Toast.makeText(applicationContext,"deneme",Toast.LENGTH_LONG)
     }
 }
