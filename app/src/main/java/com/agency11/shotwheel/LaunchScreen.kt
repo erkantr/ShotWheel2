0e9cb07427f9f992cd2e6a49e2fdaace296e5114
arincatlamaz <atlamazarinc@gmail.com>
2022-03-23 21:28:34 +0100

    LaunchScreen (backend) commit

--- app/src/main/java/com/agency11/shotwheel/LaunchScreen.kt
+++ app/src/main/java/com/agency11/shotwheel/LaunchScreen.kt
@@ -1,12 +1,18 @@
 package com.agency11.shotwheel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.agency11.shotwheel.databinding.ActivityLaunchScreenBinding
+import java.lang.Exception
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
 
 class LaunchScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivityLaunchScreenBinding
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,13 +20,21 @@ class LaunchScreen : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        thread(start = true){
 
+            try {
+                synchronized(this){
+                    Thread.sleep(3000)
+                }
+                } catch (e : InterruptedException){
+                    e.printStackTrace()
+                } finally {
+                    //val intent = Intent(this@LaunchScreen, PrepareScreen::class.java)
+                    startActivity(Intent(this@LaunchScreen, PrepareScreen::class.java))
+                }
 
+            }
 
-
-
-
-
+        }
 
     }
-}
---
+++ app/src/main/java/com/agency11/shotwheel/PrepareScreen.kt
@@ -0,0 +1,11 @@
+package com.agency11.shotwheel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrepareScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prepare_screen)
+    }
+}
